af3170a5a8ad1c11997c77c570285db1a89f3ddc
RudovEgor <91718087+RudovEgor@users.noreply.github.com>
2022-10-16 11:13:51 +0300

    Добавьте файлы проекта.

---
+++ MindBoxTask/MindBoxTask.Tests/MindBoxTest.cs
@@ -0,0 +1,57 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MindBoxTask.Tests
+{
+    [TestClass]
+    public class MindBoxTest
+    {
+        [TestClass]
+        public class CircleTest
+        {
+            [TestMethod]
+            public void Square_12_Return_452_4()
+            {
+                //arrange
+                Circle circle = new Circle("Circle", 12);
+                double expected = 452.4;
+
+                //act
+                double result = circle.CalcSquare();
+
+                //assert
+                Assert.AreEqual(expected, result);
+            }
+        }
+
+        [TestClass]
+        public class TriangleTest
+        {
+            [TestMethod]
+            public void Square_4_4_5_Return_7_8()
+            {
+                //arrange
+                Triangle triangle = new Triangle("Triangle", 4, 4, 5);
+                double expected = 7.8;
+
+                //act
+                double result = triangle.CalcSquare();
+
+                //assert
+                Assert.AreEqual(expected, result);
+            }
+
+            [TestMethod]
+            public void isRectangle_NotRectangle_ReturnFalse()
+            {
+                //Arrange
+                var triangle = new Triangle("Triangle", 2, 3, 4);
+
+                //Act
+                var result = triangle.IsRectangular();
+
+                //Assert
+                Assert.IsFalse(result);
+            }
+        }
+    }
+}
---
+++ MindBoxTask/MindBoxTask/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MindBoxTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle("Круг", 12);
+            Triangle triangle = new Triangle("Квадрат", 4, 4, 5);
+            
+            Console.WriteLine("Площадь круга = "+circle.CalcSquare());
+            Console.WriteLine("Площадь квадрата = " + triangle.CalcSquare());
+            Console.ReadKey();
+        }
+    }
+}
